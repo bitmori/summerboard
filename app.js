32883530b5c1bddf49b2a9e82a5cdf84773efabd
neonmori <neonmori@gmail.com>
2014-02-20 14:07:46 -0600

    a bit change to adapt to nodejitsu new policy

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ var server = http.createServer(app);
 app.set('port', process.env.PORT || 3000);
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
+//app.set('view engine', 'jade');
 app.use(express.favicon());
 app.use(express.logger('dev'));
 app.use(express.bodyParser());
